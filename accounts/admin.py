47c792174a330ec8bae278e5dfb8918bcf3207c5
Nana-Orkoh Dwamena <nanaorkohdwamena@gmail.com>
2023-07-09 21:27:08 +0000

    Register Model in Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("school_name","email", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "school_name","password1", "password2"),
+        }),
+    )
+    list_display = ("email", "school_name", "is_active","is_staff")
+    search_fields = ("email", "school_name")
+    ordering = ("email",)
+
+admin.site.register(User, CustomUserAdmin)
