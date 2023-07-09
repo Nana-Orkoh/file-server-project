6d41d1f60e8ffa4f1da753f3e73db5be6b3e8b73
Nana-Orkoh Dwamena <nanaorkohdwamena@gmail.com>
2023-07-09 21:30:45 +0000

    User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+import random
 
-# Create your models here.
+def generate_email_verification_code():
+        chars = '0123456789'
+        return ''.join(random.choices(chars, k=7))
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    objects = UserManager()
+    email = models.EmailField(unique=True)
+    email_verification_code = models.CharField(max_length=8, null=True, blank=True)
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.self.email
