a0e4905ddb97a48f2ccd05ea53ef865fcef6ce27
Nana-Orkoh Dwamena <nanaorkohdwamena@gmail.com>
2023-07-12 09:20:02 +0000

    Registered Files app in admin

--- files/admin.py
+++ files/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Document
 
-# Register your models here.
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description','total_downloads', 'total_emails_sent', 'uploaded_at',)
+    list_filter = ('uploaded_at', 'total_downloads', 'total_emails_sent')
+    search_fields = ('title', 'description')
+
+admin.site.register(Document, DocumentAdmin)
