1adfcd23c37b9fe756177b8e04ab407be352025e
Nana-Orkoh Dwamena <nanaorkohdwamena@gmail.com>
2023-07-12 10:05:20 +0000

    Add allowed host

--- file_server_config/settings.py
+++ file_server_config/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!0=lv-c4#w)bjazue5kc=y@!umcre3tj!cpjj+97(ut-%ruilk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nanaorkoh.pythonanywhere.com',]
 
 
 # Application definition
