21675aa6b41320a3901a92ed6f4b2d061ef428fe
Nana-Orkoh Dwamena <nanaorkohdwamena@gmail.com>
2023-07-12 09:18:25 +0000

    Document Model

--- files/models.py
+++ files/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import mimetypes
 
-# Create your models here.
+class Document(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    file = models.FileField(upload_to='documents/')
+    total_downloads = models.PositiveIntegerField(default=0)
+    total_emails_sent = models.PositiveIntegerField(default=0)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-uploaded_at',]
+
+    def get_file_type(self):
+        file_path = self.file.path
+        file_type, _ = mimetypes.guess_type(file_path)
+        if file_type:
+            return file_type.split('/')[0]
+       
+    def __str__(self):
+        return self.title
